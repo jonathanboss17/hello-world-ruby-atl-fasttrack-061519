b4309a54df6f332cec53253724b4c174b9ecc3db
dynamic-router-3131 <jtboss47@gmail.com>
2019-06-16 14:21:55 +0000

    Automatically backed up by Learn

--- hello_world.rb
+++ hello_world.rb
@@ -1,1 +1,63 @@
+#string
 puts "Hello World!"
+
+
+word = "I"
+word2 = "like"
+word3 = "cheese"
+
+#.length = length of string
+  #puts word2.length-1 #checks out ... can change how much to reduce length
+
+# for substring 
+ #puts word2[0, 2] #starts from 0 goes to 1 --> li
+ #puts word2[1] #prints out single letter
+
+
+#puts word + " "+ word2 + " " + word3
+
+#int
+number = 7.0
+
+#addition
+#puts number + 6 
+
+
+#boolean
+cheese = false; 
+
+#to run a program type "ruby _____.rb" 
+#to create a new file --> "touch _____.rb"
+#to submit to GitHub --> "learn submit" in terminal
+#"irb" into terminal allows you to test your code in the terminal for free (cos of implicit return) ... type "exit" to leave
+
+#use double to get decimal result**
+#Math Operations: +,-,*, / , % = mod, ** = exponent
+#mod for even, odd
+#puts number % 6
+
+
+#Array
+  #can have ints, strings, etc. in same array
+array = [1, 2, 3]
+
+#loop through array 
+#for i in array do 
+#  puts i 
+#end
+
+#print even numbers
+# i = 1
+# loop do
+# i += 
+#   puts i 
+# if i == 11
+#   break 
+# end
+# end
+
+
+
+
+
+
